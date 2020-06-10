87a448590c06efaddcc07f0c68b00da79ace204b
Gao Ruifeng <gaoruifeng@users.noreply.github.com>
2020-06-10 17:55:58 +0800

    Add files via upload

---
+++ 0003.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        longest = 0
+        q = deque()
+        for c in s:
+            if c in q:
+                longest = max(longest, len(q))
+                while c != q.popleft():
+                    pass
+            q.append(c)
+        return max(longest,len(q))
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.lengthOfLongestSubstring('abcabcbbdefg'))
